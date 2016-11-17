90906937e2a4fc6b7958477681dd4ee5a4da0c1e
txb <919548783@qq.com>
2016-11-17 08:34:42 +0800

    concat function

--- TianXiaoBo-lodash.js
+++ TianXiaoBo-lodash.js
@@ -373,4 +373,37 @@ TianXiaoBo = {
 		}
 		return result
 	},
+	/**
+	 * 创建一个新数组，将array与任何数组 或 值连接在一起。
+	 * 参数
+		* array (Array): 被连接的数组。
+		* [values] (...*): 连接的值。
+	 * 返回值
+		* (Array): 返回连接后的新数组。
+	 * 例子
+		* var array = [1];
+		* var other = concat(array, 2, [3], [[4]]);
+		* console.log(other);
+		* // => [1, 2, 3, [4]]
+		* console.log(array);
+		* // => [1]
+	**/
+	concat: function(arr){
+		var result = []
+		var tmp = []
+		var len = arr.length
+		var lenA = arguments.length
+		for(var i=0; i<len; i++){
+			result.push(arr[i])
+		}
+		for(var i=1; i<lenA; i++){
+			tmp.push(arguments[i])
+		}
+		var lenTmp = tmp.length
+		tmp = this.flatten(tmp)
+		for(var i=0; i<lenTmp; i++){
+			result.push(tmp[i])
+		}
+		return result
+	},
 }
