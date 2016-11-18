df5ff9bd75bd7b9d353bf92b8717e253b3dff082
txb <919548783@qq.com>
2016-11-18 15:22:37 +0800

    update

--- TianXiaoBo-lodash.js
+++ TianXiaoBo-lodash.js
@@ -1338,9 +1338,9 @@ TianXiaoBo = {
 		return result
 	}
 	reduce: function(arr, fn, init){
-		var redult = fn(arr[0],init)
+		var result = fn(arr[0],init)
 		for(var i=0; i<arr.length; i++){
-			redult = fn(arr[i].redult)
+			result = fn(arr[i],result)
 		}
 		return result
 	}
