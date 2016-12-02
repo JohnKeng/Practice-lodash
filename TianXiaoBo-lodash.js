8e9e466bc0347de1242125d217bef54ec90e3b0f
txb <919548783@qq.com>
2016-12-02 20:57:18 +0800

    update keyBy function

--- TianXiaoBo-lodash.js
+++ TianXiaoBo-lodash.js
@@ -3299,6 +3299,27 @@ TianXiaoBo = {
         }
         return result
     },
+    /**
+     * 创建一个对象组成， key（键） 是 collection（集合）中的每个元素经过 iteratee（迭代函数） 处理后返回的结果。 每个 key（键）对应的值是生成key（键）的最后一个元素。iteratee（迭代函数）调用1个参数：(value)。
+     * @param  collection (Array|Object): 用来迭代的集合。
+     * @param  [iteratee=_.identity] (Array|Function|Object|string): 这个迭代函数用来转换key。
+     * @return (Object): 返回一个组成聚合的对象。
+     */
+    keyBy: function(colle, iter) {
+        var result = {}
+        if (this.isString(iter)) {
+            var fn = function(obj) {
+                return obj[iter]
+            }
+        }
+        if (this.isFunction(iter)) {
+            var fn = iter
+        }
+        for (var key in colle) {
+            result[fn(colle[key])] = colle[key]
+        }
+        return result
+    },
 
 
 
